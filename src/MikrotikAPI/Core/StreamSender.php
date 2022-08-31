6f1edb42b65093baa9c633e3010f77a4ccfd9e0b
ariyanshipuofficial <ariyanshipuofficial@gmail.com>
2022-08-31 21:25:20 +0600

    update

--- src/MikrotikAPI/Core/StreamSender.php
+++ src/MikrotikAPI/Core/StreamSender.php
@@ -1,4 +1,14 @@
 <?php
+/*
+*
+ * @author      Ariyan Shipu 
+ * @email ariyanshipuoffical@gmail.com 
+ * @url <https://github.com/ariyanshipuofficial>
+ * @copyright   Copyright (c) 2011, Virtual Think Team.
+ * @license     http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @category	Libraries
+ * 
+ */
 
 namespace MikrotikAPI\Core;
 
